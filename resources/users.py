c6d1ed59af5de8f37246ba21f39b93529595364d
Keanu Allridge <kallridge92@gmail.com>
2019-08-15 13:48:06 -0700

    Add user delete method

--- resources/users.py
+++ resources/users.py
@@ -1,4 +1,4 @@
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import create_access_token, create_refresh_token, fresh_jwt_required
 from flask_restful import Resource
 from helpers import init_parser
 from models.users import UserModel
@@ -22,6 +22,25 @@ class UserRegister(Resource):
         new_user.save_to_db()
 
         return {'message': 'User created successfully'}, 201
+
+    # /delete - {int:user_id}
+    @fresh_jwt_required
+    def delete(self):
+        parser = init_parser()
+        parser.add_argument('user_id', type=int,
+                            required=True, help="User ID is required")
+        data = parser.parse_args()
+
+        user_to_delete = UserModel.find_by_id(data['user_id'])
+
+        if user_to_delete:
+            try:
+                user_to_delete.delete_from_db()
+                return {'message': 'Account deleted'}, 200
+            except:
+                return {'message': 'Unable to delete account'}, 500
+
+        return {'message': 'Unable to locate user'}, 404
 
 
 class UserLogin(Resource):
