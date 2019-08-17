dcf5f4d5c29487931aed0b4fd9dee4ef9f0dfbd3
Keanu Allridge <kallridge92@gmail.com>
2019-08-16 19:54:00 -0700

    Set modification tracking to false

--- settings.py
+++ settings.py
@@ -3,5 +3,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///data.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG')
